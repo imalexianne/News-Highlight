da58cf29f1c4f6dd79d85481b493772b549a9d8f
imalexianne <imanirax@yahoo.fr>
2019-02-19 16:21:04 +0200

    add article function

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
-from .request import get_sources
-
+from .request import get_sources, get_articles
+# from .request import get_articles
 # Views
 
 # Views
@@ -17,26 +17,13 @@ def index():
     technology_source = get_sources('technology')
     title = 'Home - Welcome to The best New-Highlight Website Online'
     return render_template('index.html', title = title, general = general_sources, sports = sports_source, technology = technology_source )
-# @app.route('/source/<source_id>')
-# def source(source_id):
-
-#     '''
-#     View source page function that returns the source details page and its data
-#     '''
-#     return render_template('source.html',id = source_id)
 
 
-
-
-# @app.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     # Getting popular movie
-#     popular_movies = get_movies('popular')
-#     print(popular_movies)
-#     title = 'Home - Welcome to The best Movie Review Website Online'
-#     return render_template('index.html', title = title,popular = popular_movies)
+@app.route('/article/<id>')
+def article(id):
+  '''
+  View root page function that returns the index page and its data
+  '''
+  articles = get_articles(id)
+  print(articles)
+  return render_template('article.html', articles =  articles)
